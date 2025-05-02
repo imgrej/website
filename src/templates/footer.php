9b94bea9c425bea2bfd54236fcfb7e3da4c6d15a
grej <mathias-toft@live.dk>
2025-05-02 13:42:06 +0200

    fix: Correct copyright name in footer and add MIT License

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -1,5 +1,5 @@
 <footer>
     <div class="footer-content">
-        <p>&copy; <?= date("Y") ?> grej. All rights reserved.</p>
+        <p>&copy; <?= date("Y") ?> imgrej. All rights reserved.</p>
     </div>
 </footer>
